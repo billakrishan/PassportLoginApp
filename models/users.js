65adfba246253dd4eb78cd6bef94b8e58fb794cb
Krishan Billa <billak@sheridancollege.ca>
2018-02-02 18:04:05 -0500

    first commit

---
+++ models/users.js
@@ -0,0 +1,36 @@
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/passportapp');
+
+const bcrypt = require('bcryptjs');
+
+//User schema
+
+const UserSchema=mongoose.Schema({
+  name:{
+    type:String
+  },
+  username:{
+    type:String
+  },
+  email:{
+    type:String
+  },
+  password:{
+    type:String
+  }
+});
+
+const User=module.exports=mongoose.model('User',UserSchema);
+
+module.exports.registerUser=function(newUser,callback){
+  bcrypt.genSalt(10,(err,salt)=>{
+      bcrypt.hash(newUser.password,salt,(err,hash)=>{
+          if(err){
+            console.log(err);
+          }
+          newUser.password = hash;
+          newUser.save(callback);
+      });
+  });
+
+}
